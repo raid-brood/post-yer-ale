72d8fd7f6d78ab453cb2460fd7b904f281127307
skuhlmann <samkuhlmann@gmail.com>
2024-09-18 14:04:12 -0600

    logoUrl

--- vocs.config.tsx
+++ vocs.config.tsx
@@ -1,55 +1,55 @@
-import { defineConfig } from 'vocs';
+import { defineConfig } from "vocs";
 
 export default defineConfig({
+  title: "Post Yer Ale",
+  logoUrl: "/images/steins.svg",
+  description: "Channel to cast images of your finest brews",
   ogImageUrl:
-    'https://vocs.dev/api/og?logo=%logo&title=%title&description=%description',
-  title: 'Post Yer Ale',
-  logoUrl: '/images/steins.svg',
-  description: 'Channel to cast images of your finest brews',
+    "https://vocs.dev/api/og?logo=%logoUrl&title=%title&description=%description",
   theme: {
-    accentColor: '#6DEEC0',
+    accentColor: "#6DEEC0",
     variables: {
       color: {
         background: {
-          light: 'white',
-          dark: '#272B23',
+          light: "white",
+          dark: "#272B23",
         },
       },
     },
   },
   font: {
     default: {
-      google: 'Source Code Pro',
+      google: "Source Code Pro",
     },
   },
   sponsors: [
     {
-      name: 'FRENS',
+      name: "FRENS",
       height: 120,
       items: [
         [
           {
-            name: 'POD',
-            link: 'https://pod.brood.beer',
-            image: '/images/pod.svg',
+            name: "POD",
+            link: "https://pod.brood.beer",
+            image: "/images/pod.svg",
           },
           {
-            name: 'Brood',
-            link: 'https://brood.beer',
-            image: '/images/brood.svg',
+            name: "Brood",
+            link: "https://brood.beer",
+            image: "/images/brood.svg",
           },
         ],
       ],
     },
     {
-      name: 'SPONSORS',
+      name: "SPONSORS",
       height: 120,
       items: [
         [
           {
-            name: 'DAOHaus',
-            link: 'https://daohaus.club',
-            image: '/images/daohaus.svg',
+            name: "DAOHaus",
+            link: "https://daohaus.club",
+            image: "/images/daohaus.svg",
           },
         ],
       ],
